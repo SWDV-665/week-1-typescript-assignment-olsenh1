caa9cb03a0ad4d8bfac538bc378d24b6fa53e36b
Henrik Olsen <46673574+olsenh1@users.noreply.github.com>
2019-10-21 07:58:27 -0500

    Week 1 submission
    Henrik Olsen (0913075)
    
    1.	GitHub repository URL
    https://github.com/SWDV-665/week-1-typescript-assignment-olsenh1
    2.	How many hours do you estimate you used completing this assignment?
    I spent about 5 hours on this assignment
    3.	What was easiest for you when completing this assignment?
    The easiest part for me was the programming logic. I knew pretty much from the beginning what I wanted to do and how I wanted to do it.
    4.	The most difficult challenge was the JavaScript – I am not very familiar with JavaScript code and parts of it doesn’t make much sense, in my opinion. I hope I get a chance to work more with JavaScript and become more proficient with the language. I also spent quite some time on formatting my output to look at least decent in the html file. I fought with \n (for newline) which I could not get to work. I couldn’t get <br> to work either, so I ended up switching tactics and formatted using tabs, <p> and <pre> instead.

---
+++ grocery.ts
@@ -0,0 +1,49 @@
+//SWDV-665 – Advanced Topics in Software Development
+//Maryville University, 2019
+//Week 1 Assignment: TypeScript Assignment
+//grocery.ts
+//Henrik Olsen (0913075)
+
+
+class Grocery {
+    groceryListName : string;
+	groceryList: GroceryItem[] = [];
+    constructor(public listName: string) {
+        this.groceryListName = listName;
+    }
+	add(addItem : GroceryItem) {
+		
+		this.groceryList.push(addItem);
+	}
+}
+
+
+class GroceryItem {
+    itemName: string;
+	quantity: number;
+	category: string;
+	constructor(public item: string, quantity : number, category : string) {
+		this.itemName = item;
+		this.quantity = quantity;
+		this.category = category;
+	}
+}
+
+
+let myShoppingList1 = new Grocery("Walmart");
+myShoppingList1.add(new GroceryItem("Milk, 2%  ",  1, "Dairy"))
+myShoppingList1.add(new GroceryItem("Bread     ",  2, "Bakery"))
+myShoppingList1.add(new GroceryItem("Eggs      ", 12, "Dairy"))
+myShoppingList1.add(new GroceryItem("Bananas   ",  6, "Fruit & Vegetables"))
+
+
+document.body.innerHTML = "<h1><p>" + myShoppingList1.groceryListName + "</p></h1>";
+var tempString = "";
+for(var i = 0; i < myShoppingList1.groceryListName.length; i++) {
+	tempString += "*"
+}
+document.body.innerHTML += "<h1><p>" + tempString + "</p></h1>";
+document.body.innerHTML += "<h2><strong><pre>Item &Tab;&Tab;    Quantity &Tab;&Tab;     Category</pre></strong></h2>"
+for(var i = 0;i< myShoppingList1.groceryList.length;i++) {
+	document.body.innerHTML += "<h2><p><pre>" + myShoppingList1.groceryList[i].itemName + "&Tab;&Tab;" + myShoppingList1.groceryList[i].quantity.toString() + "&Tab;&Tab;&Tab;" + myShoppingList1.groceryList[i].category + "</pre></p></h2>";
+}
